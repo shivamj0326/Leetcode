ff1ca1bb29432a06c4f6c4b4dc4b447aa20d1262
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-09-06 00:15:55 +0530

    Update 429-n-ary-tree-level-order-traversal.java

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
@@ -41,3 +41,32 @@ class Solution {
         return ans;
     }
 }
+
+/*
+DFS:
+ class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> traversal = new ArrayList<>();
+        traverse(root, traversal, 0);
+        return traversal;
+    }
+    
+    private void traverse(Node node, List<List<Integer>> traversal, int level) {
+        if (node == null) {
+            return;
+        }
+        
+        if (traversal.size() == level) {
+            traversal.add(new ArrayList<>());
+        }
+        traversal.get(level).add(node.val);
+        
+        for (Node child : node.children) {
+            traverse(child, traversal, level + 1);
+        }
+    }
+}
+*/
+
+Time Complexity : O(n)
+Space Complexity : O(n)
