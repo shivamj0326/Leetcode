0c6f526d359e8aefa72eb4d4ef579e4c8c353c76
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-09-06 00:19:05 +0530

    Time: 10 ms (6.02%), Space: 44.7 MB (12.30%) - LeetHub

---
+++ 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.java
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) {
+        
+        int depth = 0;
+        
+        if(root == null)
+            return depth;
+        
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        
+        while(!q.isEmpty()){
+            
+            List<Integer> level = new ArrayList<>();
+            int size = q.size();
+            
+            for(int i = 0 ; i < size; i++){
+                Node current = q.poll();
+                level.add(current.val);
+                
+                for(Node child : current.children){
+                    if(child != null){
+                        q.offer(child);
+                    }   
+                }
+            }
+            depth++;  
+        }
+        return depth;
+    }
+}
