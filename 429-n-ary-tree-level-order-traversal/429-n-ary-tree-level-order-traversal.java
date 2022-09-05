2133ab7fb0d288943de8f683513b469982c7dee3
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-09-06 00:09:53 +0530

    Time: 2 ms (95.52%), Space: 43.2 MB (94.52%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
@@ -19,23 +19,25 @@ class Node {
 
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
-        List<List<Integer>> traversal = new ArrayList<>();
-        traverse(root, traversal, 0);
-        return traversal;
-    }
-    
-    private void traverse(Node node, List<List<Integer>> traversal, int level) {
-        if (node == null) {
-            return;
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null)
+            return ans;
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            List<Integer> level = new ArrayList<>();
+            int size = q.size();
+            for(int i = 0 ; i < size; i++){
+                Node current = q.poll();
+                level.add(current.val);
+                for(Node child : current.children){
+                    if(child != null){
+                        q.offer(child);
+                    }   
+                }
+            }
+            ans.add(level);    
         }
-        
-        if (traversal.size() == level) {
-            traversal.add(new ArrayList<>());
-        }
-        traversal.get(level).add(node.val);
-        
-        for (Node child : node.children) {
-            traverse(child, traversal, level + 1);
-        }
+        return ans;
     }
 }
