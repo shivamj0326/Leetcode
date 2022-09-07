4fa2570b5e743b497da52d16eccc5c1f4a4482fa
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-09-08 02:56:12 +0530

    Time: 124 ms (54.69%), Space: 102.1 MB (53.93%) - LeetHub

--- 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.java
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.java
@@ -1,47 +1,26 @@
 class Solution {
     public long countExcellentPairs(int[] nums, int k) {
         Set<Integer> set = new HashSet<>();
+        long[] count = new long[32];
         long ans = 0 ;
         
         for(int num : nums){
             set.add(num);
         }
         
-        Integer[] arr = set.toArray(new Integer[set.size()]);
-        
-        Arrays.sort(arr, (a, b) -> {
-            return Integer.bitCount(a) == Integer.bitCount(b) ? a - b : Integer.bitCount(a) - Integer.bitCount(b);
-        });
-        
-        for(int i = 0 ; i < arr.length; i++){
-            arr[i] = Integer.bitCount(arr[i]);
+        for(int num : set){
+            count[Integer.bitCount(num)]++;
         }
-            
-        for(int i = 0 ; i < arr.length ; i++){
-            
-            if(arr[i] >= k)
-                ans += 2*i + 1;
-            else{
-                 int index = search(arr, i, k - arr[i]);
-                if(index != -1)
-                    ans += (i - index)*2 + 1;
-            }        
-        }
-        return ans;
-    }
-    
-    public int search(Integer[] arr,int index, int target){
-        int low = 0 , high = index, ans = -1;
         
-        while(low <= high){
-            int mid = low + (high - low)/2 ;
-            if(arr[mid] >= target){
-                ans = mid;
-                high = mid - 1;
+        for(int i = 1 ; i <= 30 ; i++){
+            for(int j = 1 ; j <= 30 ; j++){
+                if(i + j >= k){
+                    ans += count[i] * count[j];
+                }
             }
-            else
-                low = mid + 1;
         }
+        
         return ans;
+        
     }
 }
