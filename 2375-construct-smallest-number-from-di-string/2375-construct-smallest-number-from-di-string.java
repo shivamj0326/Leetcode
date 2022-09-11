3f0dc2abaa59729447600ac88d1d66ff2ff0bf43
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-09-12 03:25:04 +0530

    Time: 1 ms (91.79%), Space: 40.4 MB (91.38%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.java
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.java
@@ -1,26 +1,33 @@
 class Solution {
     public String smallestNumber(String pattern) {
+        
+        
         int n = pattern.length();
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-        for(int i = 1 ; i <= n + 1; i++){
-            q.offer(i);
-        }
-        int left = -1 ;
+        int min = 1;
+        
+        int beenSet = -1 ;
+        
+        // we need resultant string of length n + 1
         StringBuilder sb = new StringBuilder(pattern + " ");
         
         for(int i = 0 ; i < n ; i++){
+            
             char ch = pattern.charAt(i);
+            
+            //encounter I set min possible at this index
             if(ch == 'I'){
-                sb.setCharAt(i, (char)(q.poll() + 48));
-                for(int j = i - 1 ; j > left ; j--){
-                    sb.setCharAt(j, (char)(q.poll() + 48));
+                //ascii of '0' is 48
+                sb.setCharAt(i, (char)(min++ + 48));
+                // set min possible at All 'D' to the left of 'I' and not been set so far
+                for(int j = i - 1 ; j > beenSet ; j--){
+                    sb.setCharAt(j, (char)(min++ + 48));
                 }
-                left = i ;
+                beenSet = i ;
             }  
         }
-        
-        for(int i = n ; i > left ; i--){
-            sb.setCharAt(i, (char)(q.poll() + 48));
+        //for characters not been set
+        for(int i = n ; i > beenSet ; i--){
+            sb.setCharAt(i, (char)(min++ + 48));
         }
         return sb.toString();
         
