b13d459851ef276b7f8328b0ef4ce29da755f196
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-09-18 04:58:38 +0530

    Time: 25 ms (91.64%), Space: 58.6 MB (69.58%) - LeetHub

---
+++ 15-3sum/15-3sum.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> triplets = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+        
+        for(int i = 0 ; i < n ; i++){
+            int target = - nums[i];
+            int left = i + 1;
+            int right = n - 1;
+            int sum = 0 ;
+            while(left < right){
+                sum = nums[left] + nums[right];
+            
+                if(sum > target)
+                    right--;
+                else if(sum < target)
+                    left++;
+                else{
+                    triplets.add(List.of(nums[i], nums[left], nums[right]));
+                    while(left + 1 < n && nums[left + 1] == nums[left])
+                        left++;
+                    while(right - 1 >= 0 && nums[right - 1] == nums[right])
+                        right--;
+                    left++;
+                    right--;
+                }
+            }   
+            while(i + 1 < n && nums[i + 1] == nums[i])
+                i++;
+        }
+        return triplets;
+    }
+}
