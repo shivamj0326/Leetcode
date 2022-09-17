ceb6b9025a2904fd37b93741f6273f4dcbb2b3bc
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-09-18 05:11:07 +0530

    Time: 22 ms (98.59%), Space: 46.3 MB (97.14%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -4,6 +4,7 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         
+        //sorted array, hence if nums[i] positive, result cannot be zero
         for(int i = 0 ; i < n && nums[i] <= 0; i++){
             int target = - nums[i];
             int left = i + 1;
@@ -18,15 +19,22 @@ class Solution {
                     left++;
                 else{
                     triplets.add(List.of(nums[i], nums[left], nums[right]));
+                    //avoid duplicate in left term
                     while(left + 1 < n && nums[left + 1] == nums[left])
                         left++;
                     left++;
                     right--;
                 }
             }   
+            //Avoid duplicate in ith term
             while(i + 1 < n && nums[i + 1] == nums[i])
                 i++;
         }
         return triplets;
     }
 }
+/*
+ Time Complexity : O(nlogn)
+ Space Complexity : O(1) if output excluded 
+*/
+ 
