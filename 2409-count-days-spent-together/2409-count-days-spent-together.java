5eaa579d13d833a642cf1390c95e5b6cb9273484
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-09-18 00:28:48 +0530

    Time: 1 ms (90.00%), Space: 42.3 MB (40.00%) - LeetHub

--- 2409-count-days-spent-together/2409-count-days-spent-together.java
+++ 2409-count-days-spent-together/2409-count-days-spent-together.java
@@ -1,15 +1,19 @@
 class Solution {
     public int countDaysTogether(String arriveAlice, String leaveAlice, String arriveBob, String leaveBob) {
+        
         int days = 0 ;
         
+        //Get Max Arrival date :
         String arrive = arriveAlice.compareTo(arriveBob) < 0 ? arriveBob : arriveAlice;
         
+        //Get Min Arrival date:
         String depart = leaveAlice.compareTo(leaveBob) <= 0 ? leaveAlice : leaveBob;
         
         int monthArrive = Integer.parseInt(arrive.substring(0, 2));
         
         int monthLeave = Integer.parseInt(depart.substring(0, 2));
         
+        //if arrive month > leave month return 0
         if(monthArrive > monthLeave)
             return 0 ;
         
@@ -17,26 +21,26 @@ class Solution {
         
         int dayLeave = Integer.parseInt(depart.substring(3, 5));
         
-
-        System.out.println(dayArrive + " " + dayLeave);
-        
         int[] months = new int[]{0,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
       
         if(monthArrive == monthLeave){
+            //if arrive and leave month same return 0 if dayArrive > dayLeave
             if(dayArrive > dayLeave)
                 return 0 ;
             else
                 return dayLeave - dayArrive + 1; 
         }    
         else{
+            //calculate days excluding arrive month and end month
             for(int i = monthArrive + 1 ; i < monthLeave ; i++){
                 days += months[i];
             }
+            //arrive month days will be total days in arrive month - dayArrive + 1
             days += months[monthArrive] - dayArrive + 1;
+            //leave days will be simply dayleave
             days += dayLeave;
             return days;
         }
-         
-        
+               
     }
 }
