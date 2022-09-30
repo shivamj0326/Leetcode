8c5fc93f509b04a81394c1f1bd59fd1299e72868
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-10-01 03:28:16 +0530

    Time: 1 ms (98.03%), Space: 41.9 MB (78.47%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -4,15 +4,12 @@ class Solution {
         int duplicates = 0 , n = s.length(), ans = 0;
         for(int i = 0 ; i < n ; i++){
 
-            if(count[s.charAt(i) - 'a'] == 1)
+            if(++count[s.charAt(i) - 'a'] == 2)
                 duplicates++;
-            count[s.charAt(i) - 'a']++;
-            
-            
-            if(i >= 3){
-                if(count[s.charAt(i - 3) - 'a'] == 2)
+            if(i < 2)
+                continue;
+            if(i >= 3 && --count[s.charAt(i - 3) - 'a'] == 1){
                     duplicates--;
-                count[s.charAt(i - 3) - 'a']--;
             }
             if(i >= 2 && duplicates == 0)
                 ans++;
