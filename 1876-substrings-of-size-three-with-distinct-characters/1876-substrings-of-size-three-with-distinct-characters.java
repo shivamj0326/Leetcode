0aa53e1b4bf55191b79d58234ca2d8fe6683b1f9
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-10-01 03:25:54 +0530

    Time: 2 ms (71.50%), Space: 41.9 MB (78.47%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,29 +1,25 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        
-        int n = s.length();
-        char prev ;
-        
-        if(n < 3)
-            return 0 ;
-        
-        for(int i = 0 ; i <= 2 ; i++){
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-        int count = map.size() == 3 ? 1 : 0;
-        for(int i = 3 ; i < s.length(); i++){
-            char ch = s.charAt(i);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+       int[] count = new int[26];
+        int duplicates = 0 , n = s.length(), ans = 0;
+        for(int i = 0 ; i < n ; i++){
+
+            if(count[s.charAt(i) - 'a'] == 1)
+                duplicates++;
+            count[s.charAt(i) - 'a']++;
             
-            prev = s.charAt(i - 3);
-            map.put(prev, map.get(prev) - 1);
-            if(map.get(prev) == 0)
-                map.remove(prev);
-            if(map.size() == 3)
-                count++;
+            if(i < 2)
+                continue;
+            
+            if(i >= 3){
+                if(count[s.charAt(i - 3) - 'a'] == 2)
+                    duplicates--;
+                count[s.charAt(i - 3) - 'a']--;
+            }
+            if(i >= 2 && duplicates == 0)
+                ans++;
         }
-        return count;
+        
+        return ans;
     }
 }
