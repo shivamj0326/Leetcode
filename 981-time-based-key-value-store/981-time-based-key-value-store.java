8532ec3b89735ca44c63d1400ae05ee76e795a30
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-10-07 01:11:53 +0530

    Time: 396 ms (33.08%), Space: 217.1 MB (60.49%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -1,40 +1,28 @@
 class TimeMap {
     
-    Map<String, String> keyMap = new HashMap<>();
+    private Map<String, TreeMap<Integer, String>> keyMap ;
     
-    Map<String, TreeSet<Integer>> timeMap = new HashMap<>();
-
     public TimeMap() {
         keyMap = new HashMap<>();
     }
     
     public void set(String key, String value, int timestamp) {
-        keyMap.put(key + "#" + timestamp, value);
-        if(timeMap.containsKey(key)){
-            TreeSet<Integer> timeset = timeMap.get(key);
-            timeset.add(timestamp);
-            timeMap.put(key, timeset);
-        }
-        else{
-            TreeSet<Integer> timeset = new TreeSet<>();
-            timeset.add(timestamp);
-            timeMap.put(key, timeset);
-        }
+       TreeMap<Integer, String> timeMap = keyMap.get(key);
+        if(timeMap == null)
+            timeMap = new TreeMap<Integer, String>();
+        timeMap.put(timestamp, value);
+        keyMap.put(key, timeMap);
             
     }
     
     public String get(String key, int timestamp) {
-        if(keyMap.containsKey(key + "#" + timestamp))
-            return keyMap.get(key + "#" + timestamp);
-        else{
-            TreeSet<Integer> set = timeMap.get(key);
-            if(set == null)
-                return "";
-            Integer time = set.floor(timestamp);
-            if(time == null)
-                return "";
-            return keyMap.get(key + "#" + time);
-        }
+        if(!keyMap.containsKey(key))
+            return "";
+        TreeMap<Integer, String> timeMap = keyMap.get(key);
+        Integer time = timeMap.floorKey(timestamp);
+        if(time == null)
+            return "";
+        return timeMap.get(time);
     }
 }
 
