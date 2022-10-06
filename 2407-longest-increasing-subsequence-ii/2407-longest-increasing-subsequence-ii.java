95810dcc2e25a71176568de84b65b69aa6656be4
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-10-06 21:05:22 +0530

    Time: 168 ms (63.06%), Space: 50.8 MB (96.75%) - LeetHub

--- 2407-longest-increasing-subsequence-ii/2407-longest-increasing-subsequence-ii.java
+++ 2407-longest-increasing-subsequence-ii/2407-longest-increasing-subsequence-ii.java
@@ -1,64 +1,46 @@
-class SegmentTree{         
-        private int[] tree;
-        private int n;
-        //init tree
-        public SegmentTree(int n){
-            this.n=n;
-            
-            tree=new int[4 * n];
-        }
-        
-        public int query(int left,int right){
-            return query(0,0,n-1,left,right);
-        }
-        
-        private int query(int idx,int l,int r,int left,int right){
-            if(r<left||right<l){
-                return 0;
-            }else if(left<=l && r<=right){
-                return tree[idx];
-            }else{
-                int m=l+(r-l)/2;
-                int lMax=query(2*idx+1,l,m,left,right);
-                int rMax=query(2*idx+2,m+1,r,left,right);
-                return Math.max(lMax,rMax);
-            }            
-        }
-        
-        public void update(int idx,int val){
-            update(0,0,n-1,idx,val);
-        }
-        
-        private void update(int idx,int l,int r,int updateIdx,int val){
-            if(l==r){
-                tree[idx]=Math.max(tree[idx],val);    
-                return;
-            }
-            
-            int m=l+(r-l)/2;
-            if(l<=updateIdx && updateIdx<=m){
-                update(2*idx+1,l,m,updateIdx,val);
-            }else {
-                update(2*idx+2,m+1,r,updateIdx,val);
-            }
-            tree[idx]=Math.max(tree[2*idx+1],tree[2*idx+2]);
-        }
-    
-        public int max(){
-            return tree[0];
-        }
-}
-    
-
-class Solution {     
+class Solution {
+    int[] segmentTree;
+    int n ;
     public int lengthOfLIS(int[] nums, int k) {
-        SegmentTree segmentTree=new SegmentTree((int)(1e5)+1);
-        for(int a:nums){
-            int left= Math.max(0,a-k);
-            int right= a-1;
-            int currMax = segmentTree.query(left,right);
-            segmentTree.update(a,currMax+1);
-        }        
-        return segmentTree.max();
+        n = nums.length;
+        segmentTree = new int[4 * 100005];
+        
+        for(int i = 0 ; i < n ; i++){
+            int low = Math.max(0, nums[i] - k);
+            int count = query(0, 0, 100000, low, nums[i] - 1);
+            update(0, 0, 100000, nums[i], count + 1);
+        }
+        
+        return segmentTree[0];
+    }
+    
+    private void update(int pos, int left, int right, int index, int val){
+        if(left == right){
+            segmentTree[pos] = Math.max(segmentTree[pos], val);
+            return;
+        }
+        
+        int mid = left + (right - left)/2 ;
+        
+        if(index > mid)
+            update(2 * pos + 2, mid + 1, right, index, val);
+        else
+            update(2 * pos + 1, left, mid, index, val);
+        
+        segmentTree[pos] = Math.max(segmentTree[pos], Math.max(segmentTree[2 * pos + 1], segmentTree[2 * pos + 2]));
+    }
+    
+    private int query(int pos, int low, int high, int left, int right){
+        if(high < left || low > right)
+            return 0;
+        if(low >= left && high <= right)
+            return segmentTree[pos];
+        
+        int mid = low + (high - low)/2 ;
+        
+        int l = query(2 * pos + 1, low, mid, left, right);
+        int r = query(2 * pos + 2, mid + 1, high, left, right);
+        
+        return Math.max(l, r) ;
     }
 }
