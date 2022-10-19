7bfa63daf6bef939ec40f5e636ddb1b74e9aea6d
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-10-20 02:27:11 +0530

    Time: 127 ms (56.75%), Space: 54.3 MB (65.40%) - LeetHub

--- 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.java
+++ 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.java
@@ -1,59 +1,75 @@
 class Solution {
     public boolean[] friendRequests(int n, int[][] restrictions, int[][] requests) {
-        boolean[] ans = new boolean[requests.length];
         UnionFind uf = new UnionFind(n);
         
-        for(int i=0; i<requests.length; i++) {
-            int u = requests[i][0], v = requests[i][1];
-            int up = uf.find(u), vp = uf.find(v);
-            ans[i] = true;
-            
-            for(int[] restriction : restrictions) {
-                int x = restriction[0], y = restriction[1];
-                int xp = uf.find(x), yp = uf.find(y);
-                
-                if( (xp == up && yp == vp) || (yp == up && xp == vp) ) {
-                    ans[i] = false;
-                    break;
-                } 
+        boolean[] ans = new boolean[requests.length];
+        int i = 0 ;
+        
+    
+        for(int[] request : requests){
+            int m = request[0];
+            int c = request[1];
+            int x = uf.find(m);
+            int y = uf.find(c);
+            boolean valid = true;
+            if(!uf.isConnected(request[0], request[1])){
+                for(int[] res : restrictions){
+                    int p = uf.find(res[0]);
+                    int q = uf.find(res[1]);
+                    if((x == p && y == q) || (x == q && y == p)){
+                        valid = false;
+                        break;
+                    }         
+                }
             }
-            if( ans[i] )
-                uf.union(up, vp);
+            ans[i++] = valid; 
+            if(valid)
+                uf.union(m, c);
         }
+       
         return ans;
     }
 }
-class UnionFind {
-    private int[] root, rank;
+
+class UnionFind{
+    private int[] root;
+    private int[] rank;
     
-    public UnionFind(int n) {
+    UnionFind(int n){
         root = new int[n];
         rank = new int[n];
         
-        for(int i=0; i<n; i++)
+        for(int i = 0 ; i < n ; i++){          
             root[i] = i;
-           
-    }
-    
-    public int find(int x) {
-        if( root[x] != x )
-            root[x] = find(root[x]);
-        return root[x];
-    }
-    
-    public boolean union(int x, int y) {
-        int px = find(x), py = find(y);
-        
-        if( px == py ) return false;
-        
-        if( rank[py] > rank[px] ) 
-            root[px] = py;
-        else if( rank[px] > rank[py] )
-            root[py] = px;
-        else {
-            root[py] = px;
-            rank[px] += 1;
         }
-        return true;
+    }
+    
+    public int find(int x){
+        if(x != root[x])
+            return root[x] = find(root[x]);
+        return root[x] ;
+    }
+    
+    public boolean isConnected(int x, int y){
+        return find(x) == find(y);
+    }
+    
+    public void union(int x, int y){
+        int rootX = find(x);
+        int rootY = find(y);
+        
+        if(rootX == rootY)
+            return;
+        
+        if(rank[rootX] > rank[rootY]){
+            root[rootY] = rootX;
+        }
+        else if(rank[rootY] > rank[rootX]){
+            root[rootX] = rootY;
+        }
+        else{
+            root[rootY] = rootX;
+            rank[rootX]++;
+        }
     }
 }
