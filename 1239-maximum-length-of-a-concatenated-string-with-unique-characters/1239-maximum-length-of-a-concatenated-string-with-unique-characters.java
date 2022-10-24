535af1a68357e5a981d5421b972768a05bceed3c
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-10-24 12:22:00 +0530

    Time: 11 ms (88.53%), Space: 46.8 MB (67.09%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,23 +1,14 @@
 class Solution {
     public int maxLength(List<String> arr) {
         
-        int max = 0 ;
-        
-        max = Math.max(max, solve(arr, 0, new int[26]));
-        
+        int max = 0 ;   
+        max = Math.max(max, solve(arr, 0, new int[26], 0));      
         return max;
-
     }
     
-    public int solve(List<String> arr, int index, int[] count){
+    public int solve(List<String> arr, int index, int[] count, int len){
         if(index == arr.size()){
-            int ans = 0 ;
-            
-            for(int i = 0 ; i < count.length ; i++){
-                ans += count[i];
-            }
-            
-            return ans;
+            return len;
         }
         
         boolean canPick = canpick(count, arr.get(index));
@@ -30,24 +21,26 @@ class Solution {
             String current = arr.get(index);
             for(char c : current.toCharArray()){
                 count[c - 'a']++;
+                len++;
             }
-            pick = solve(arr, index + 1, count);
+            pick = solve(arr, index + 1, count, len);
             for(char c : current.toCharArray()){
                 count[c - 'a']--;
+                len--;
             }
             
-            notpick = solve(arr, index + 1, count);
+            notpick = solve(arr, index + 1, count, len);
         }
         else{
-            notpick = solve(arr, index + 1, count);
+            notpick = solve(arr, index + 1, count, len);
         }
         
         return Math.max(pick, notpick);        
     }
                            
     public boolean canpick(int[] count, String s){
-        int a = 0 ;
         
+        int a = 0 ;  
         for(char c : s.toCharArray()){
             if(count[c - 'a'] == 1)
                 return false;
