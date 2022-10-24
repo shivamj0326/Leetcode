61b6ba086273dac1112bccf16026f1f83cae99c9
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-10-24 15:53:59 +0530

    Time: 221 ms (29.58%), Space: 99.6 MB (5.02%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,16 +1,17 @@
-public class Solution {
+class Solution {
     public int countPrimes(int n) {
-        boolean[] notPrime = new boolean[n];
+        int[] visited = new int[n + 1] ;
         int count = 0;
-        for (int i = 2; i < n; i++) {
-            if (notPrime[i] == false) {
-                count++;
-                for (int j = 2; i*j < n; j++) {
-                    notPrime[i*j] = true;
-                }
-            }
-        }
         
+        for(int i = 2 ; i < n ; i++){
+            if(visited[i] == 0){
+                count++;
+                for(long j = (long)i*i ; j < n ; j = j + i){
+                    visited[(int)j] = -1;   
+                }
+            }         
+        }
         return count;
+        
     }
 }
