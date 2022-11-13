2ca85003e0aefae3ef9a8e723fc6bf15b4e589a6
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-11-13 09:08:08 +0000

    Time: 0 ms (100.00%), Space: 41.7 MB (56.42%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean check(int[] nums) {
         int n = nums.length;
+        int count = 0;
         for(int i = 0 ; i < n ; i++){
-            int count = 0 ;
-            for(int j = i ; j < i + n ; j++){
-                if(nums[j % n] <= nums[(j + 1) % n])
-                    count++;
-            }
-            if(count >= n - 1)
-                return true;
+            if(nums[i] > nums[(i + 1) % n])
+                count++;
+            if(count > 1)
+                return false;
         }
-        return false;
+        return true;
     }
 }
