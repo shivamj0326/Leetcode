f9907588a7704c507d204ac2f88458d6e419435f
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-11-20 10:01:57 +0000

    Time: 102 ms (100.00%), Space: 147.2 MB (66.67%) - LeetHub

---
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.java
@@ -0,0 +1,40 @@
+class Solution {
+    long cost = 0 ;
+    int seats;
+    public long minimumFuelCost(int[][] roads, int seats) {
+        List<List<Integer>> graph = new ArrayList<>();
+        this.seats = seats;
+        
+        for(int i = 0 ; i <= roads.length; i++){
+            graph.add(new ArrayList<>());
+        }
+        
+        for(int[] road : roads){
+            graph.get(road[0]).add(road[1]);
+            graph.get(road[1]).add(road[0]);
+        }
+        
+        for(int i : graph.get(0)){
+            dfs(i, graph, 0);
+        }
+        
+        return cost;
+    }
+    
+    public int dfs(int node, List<List<Integer>> graph, int prev){
+        
+        int people = 1;
+        for(int i : graph.get(node)){
+            if(i == prev)
+                continue;
+            
+            people += dfs(i, graph, node);
+            
+        }
+        
+        cost += people/seats + (people % seats == 0 ? 0 : 1);
+        return people;
+        
+        
+    }
+}
