9055b9f30d6890c58a4154729761fbd864090c97
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-11-22 18:45:05 +0000

    Time: 108 ms (38.53%), Space: 72.1 MB (13.50%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,17 +1,30 @@
 class Solution {
     public int numSquares(int n) {
         
-        int[] dp = new int[n + 1];
+        Deque<Integer> q = new ArrayDeque<>();
+        Set<Integer> visited = new HashSet<>();
+        int level = 0 ;
         
-        for(int i = 1 ; i <= n ; i++){
-            dp[i] = i ;
-            for(int j = 1 ; j * j <= i ; j++){
-                dp[i] = Math.min(dp[i - j * j] + 1, dp[i]);
+        q.offer(n);
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            level++;
+            
+            for(int i = 0 ; i < size; i++){
+                int rem = q.poll();
+                
+                for(int j = 1 ; j * j <= rem; j++){
+                    
+                    if(rem - j * j == 0)
+                        return level;
+                    if(!visited.contains(rem - j * j)){
+                        visited.add(rem - j * j);
+                        q.offer(rem - j * j);
+                    }
+                }
             }
         }
-        
-        return dp[n];
-    }
-    
-    
+        return n;
+    }   
 }
