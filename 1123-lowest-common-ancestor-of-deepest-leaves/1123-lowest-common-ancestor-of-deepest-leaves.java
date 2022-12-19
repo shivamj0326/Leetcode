66f83b419a4f054cf0b0acf08dda2fc45ecaeb62
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2022-12-19 20:23:08 +0000

    Time: 1 ms (82.78%), Space: 45.4 MB (9.59%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,24 @@
+class Solution {
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    // Return the result of the subtree at this node.
+    public Result dfs(TreeNode node) {
+        if (node == null) return new Result(null, 0);
+        Result L = dfs(node.left),
+               R = dfs(node.right);
+        if (L.dist > R.dist) return new Result(L.node, L.dist + 1);
+        if (L.dist < R.dist) return new Result(R.node, R.dist + 1);
+        return new Result(node, L.dist + 1);
+    }
+}
+
+class Result {
+    TreeNode node;
+    int dist;
+    Result(TreeNode n, int d) {
+        node = n;
+        dist = d;
+    }
+}
