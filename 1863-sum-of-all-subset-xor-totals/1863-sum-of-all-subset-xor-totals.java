6873649a6aa35573d0df7e689722a47be67aa64a
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2023-01-08 17:00:04 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (91.78%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,24 +1,15 @@
 class Solution {
-    List<Integer> res = new ArrayList<>();
-    public int subsetXORSum(int[] nums) {
-        
-        dfs(nums, 0, 0);
-        int sum = 0;
-        for(int i : res)
-            sum += i;
-        
-        return sum;
+    
+    public int subsetXORSum(int[] nums) { 
+        return dfs(nums, 0, 0);    
     }
     
-    public void dfs(int[] nums, int index, int current){
+    public int dfs(int[] nums, int index, int current){
         if(index == nums.length){
-            res.add(current);
-            return;
+            return current;
         }
        
-        dfs(nums, index + 1, current ^ nums[index]);
-        
-        dfs(nums, index + 1, current);
+        return dfs(nums, index + 1, current ^ nums[index]) + dfs(nums, index + 1, current);
 
     }
 }
