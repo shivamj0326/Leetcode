6bd9115c14d34733f7604d2d99cf7f8615c10b1a
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2023-01-08 16:01:19 +0530

    Time: 2 ms (97.40%), Space: 41.8 MB (63.64%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.java
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.java
@@ -1,6 +1,8 @@
 class Solution {
     int mod = 1000000007;
     public int numMusicPlaylists(int n, int goal, int k) {
+        
+        //dp[i][j] denotes n unique songs in playlist of j songs
         long[][] dp = new long[n + 1][goal + 1];
         
         long[] fact = new long[n + 1];
@@ -13,7 +15,8 @@ class Solution {
         for(int i = k + 1; i <= n; i++){
             for(int j = i ; j <= goal; j++){
                 
-                if(i == j || i == k + 1)
+                //if
+                if(i == j)
                     dp[i][j] = fact[i];
                 else
                     dp[i][j] = (dp[i - 1][j - 1] * i + dp[i][j - 1] * (i - k))%mod;
