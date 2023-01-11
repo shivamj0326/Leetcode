5f6ac9d257727b9fafa7660f534f7342047282fa
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2023-01-12 00:28:52 +0530

    Time: 33 ms (88.24%), Space: 85.5 MB (64.12%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.java
@@ -1,47 +1,33 @@
 class Solution {
-    int res = 0 ;
+    
     public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
         List<List<Integer>> graph = new ArrayList<>();
-        
+    
         for(int i = 0 ; i < n ; i++){
             graph.add(new ArrayList<Integer>());
         }
-        
         for(int[] e : edges){
             graph.get(e[0]).add(e[1]);
             graph.get(e[1]).add(e[0]);
         }
-        dfs1(graph, hasApple, 0, -1);
-     
-        dfs2(graph, hasApple, 0, -1);
-        return 2 * res;
         
+        return  dfs(graph, hasApple, 0, -1);
         
     }
     
-    public boolean dfs1(List<List<Integer>> graph, List<Boolean> hasApple, int src, int par){
+    public int dfs(List<List<Integer>> graph, List<Boolean> hasApple, int src, int par){
+        int total = 0 ;
         boolean res = hasApple.get(src);
         for(int i : graph.get(src)){
             if(i == par)
                 continue;
-            res |= dfs1(graph, hasApple, i, src);
+            int child = dfs(graph, hasApple, i, src);
+            
+            if(child > 0 || hasApple.get(i))
+                total += 2 + child;
             
         }
         
-        hasApple.set(src, res);
-        
-        return res;
-    }
-    
-    public void dfs2(List<List<Integer>> graph, List<Boolean> hasApple, int src, int par){
-        for(int i : graph.get(src)){
-            if(i == par)
-                continue;
-            
-            if(hasApple.get(i)){
-                res++;
-                dfs2(graph, hasApple, i, src);
-            }
-        }
+        return total;
     }
 }
