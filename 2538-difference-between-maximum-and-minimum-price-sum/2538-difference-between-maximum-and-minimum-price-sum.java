6d6f801fd69b5a09a93d4cddac23facc4efdbbaf
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2023-01-15 10:29:13 +0530

    Time: 236 ms (50.00%), Space: 122.9 MB (100.00%) - LeetHub

---
+++ 2538-difference-between-maximum-and-minimum-price-sum/2538-difference-between-maximum-and-minimum-price-sum.java
@@ -0,0 +1,43 @@
+class Solution {
+    private long max;
+    private ArrayList<Integer>[] tree;
+    private int[] price;
+    private HashMap<Long, Long> memo;
+    
+    public long maxOutput(int n, int[][] edges, int[] price) {
+        this.price = price;
+        memo = new HashMap<>();
+        tree = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            tree[i] = new ArrayList<>();
+        }
+        for (int[] e : edges) {
+            tree[e[0]].add(e[1]);
+            tree[e[1]].add(e[0]);
+        }
+        
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            if (tree[i].size() == 1) {
+                long temp = helper(-1, i);
+                res = Math.max(res, temp - price[i]);
+            }
+        }
+        return res;
+    }
+    
+    private long helper(int parent, int node) {
+        long key = (long)(parent + 1) * 1_000_000 + node;
+        if (memo.containsKey(key)) return memo.get(key);
+        
+        long res = price[node];
+        for (int child : tree[node]) {
+            if (child != parent) {
+                res = Math.max(res, price[node] + helper(node, child));
+            }
+        }
+        
+        memo.put(key, res);
+        return res;
+    }
+}
