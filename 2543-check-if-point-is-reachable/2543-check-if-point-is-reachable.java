c3398cefb12062535a47c0329a2a420f1174562c
shivamj0326 <103810364+shivamj0326@users.noreply.github.com>
2023-01-21 23:23:17 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (100.00%) - LeetHub

--- 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.java
+++ 2543-check-if-point-is-reachable/2543-check-if-point-is-reachable.java
@@ -1,23 +1,27 @@
 class Solution {
     public boolean isReachable(int x, int y) {
         
-        while(x != 1 || y != 1){
+        while(x != 1 && y != 1){
+            
             x = divideBy2(x);
             y = divideBy2(y);
+            
             if(x == 1 || y == 1)
                 return true;
+            
             else if(x == y)
                 return false;
+            
             else if(x < y){
                 y = x + y;
             }
+            
             else{
                 x = x + y;
             }
-        }
-        
-        return true;
-        
+            
+        } 
+        return true;   
     }
     
     int divideBy2(int x){
